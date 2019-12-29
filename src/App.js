3df68de7d02808ac4d25bd948a6dc5c49f97b82d
sevenish7 <evans.samuel7@gmail.com>
2019-12-29 11:16:51 +1100

    added basic sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInPage from './pages/signin/signin-page.component'
 
 const HatsPage = () => (
     <div>
@@ -18,6 +19,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
+            <Route path='/signin' component={SignInPage}/>
         </Switch>
     </div>
   );
---
+++ src/pages/signin/signin-page.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/signin/signin.component'
+import './signin-page.styles.scss'
+
+const SignInPage = () => (
+    <div className  = 'signin-page'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInPage;
