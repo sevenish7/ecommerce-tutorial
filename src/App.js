3ae571a1fb2993aa9a32e9e1f43e8bf287090510
sevenish7 <evans.samuel7@gmail.com>
2020-02-17 20:21:54 +1030

    added email and password with firebase signup

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends React.Component {
                         }
                     });
                 });
+
+                console.log(this.state);
             } else {
                 this.setState({currentUser: userAuth});
             }
--- src/pages/signin/signin-page.component.jsx
+++ src/pages/signin/signin-page.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import SignIn from '../../components/signin/signin.component'
-import './signin-page.styles.scss'
+import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+import './signin-page.styles.scss';
 
 const SignInPage = () => (
     <div className  = 'signin-page'>
         <SignIn/>
+        <SignUp/>
     </div>
 );
 
